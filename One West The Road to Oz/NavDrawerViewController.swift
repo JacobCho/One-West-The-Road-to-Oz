d69508a920d44cbea09b0a76e9e35bfd9fb199e1
Jacob Cho <jacobshcho@gmail.com>
2014-12-03 15:07:56 -0800

    Nav bar styling

--- One West The Road to Oz/NavDrawerViewController.swift
+++ One West The Road to Oz/NavDrawerViewController.swift
@@ -15,15 +15,24 @@ class NavDrawerViewController: UIViewController, UITableViewDataSource, UITableV
     @IBOutlet weak var tableView: UITableView!
     
     var navArray : Array<String> = ["Workouts", "Information", "TT Results", "Settings"]
+    var iconArray :Array<UIImage>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupProfileImage()
+        setupIcons()
     }
     
     func setupProfileImage() {
-        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width/2
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width/4
         self.profileImageView.clipsToBounds = true
+    }
+    
+    func setupIcons() {
+        
+        var workoutIcon : UIImage = UIImage(named: "WorkoutIcon")!
+        iconArray = [workoutIcon, workoutIcon, workoutIcon, workoutIcon]
+        
     }
     
     // MARK: Table View Data Source
@@ -41,8 +50,9 @@ class NavDrawerViewController: UIViewController, UITableViewDataSource, UITableV
         var cell : NavDrawerTableCell = tableView.dequeueReusableCellWithIdentifier("navDrawerCell", forIndexPath: indexPath) as NavDrawerTableCell
         
         let navItem = navArray[indexPath.row]
-        
+        let icon = UIImage(named:"WorkoutIcon")
         cell.navLabel.text = navItem
+        cell.iconImageView.image = icon
         
         return cell
 
