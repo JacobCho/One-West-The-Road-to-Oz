50be92ba9a7047ffc9c216661d01a3bea248dc31
Jacob Cho <jacobshcho@gmail.com>
2014-12-09 15:18:53 -0800

    Updated views for iPhone 5

---
+++ One West The Road to Oz/WorkoutsTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutsTabBarController.swift
+//  One West The Road to Oz
+//
+//  Created by Jacob Cho on 2014-12-08.
+//  Copyright (c) 2014 Jacob. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutsTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = UIColor(red: 244.0/255.0, green: 67.0/255.0, blue: 54.0/255.0, alpha: 1)
+    }
+
+}
